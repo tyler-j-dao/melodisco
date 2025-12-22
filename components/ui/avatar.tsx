347d2bd317b6b7a03c44b087fa9e575c40003dcb
seniorDev812 <cassianrourke@gmail.com>
2025-12-22 07:37:30 -0800

    remove the avatar image

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -8,15 +8,7 @@ import { cn } from "@/lib/utils"
 
 Avatar.displayName = AvatarPrimitive.Root.displayName
 
-const AvatarImage = React.forwardRef<
-  React.ElementRef<typeof AvatarPrimitive.Image>,
-  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
->(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Image
-    ref={ref}
-    className={cn("aspect-square h-full w-full", className)}
-    {...props}
-  />
+
 ))
 AvatarImage.displayName = AvatarPrimitive.Image.displayName
 
